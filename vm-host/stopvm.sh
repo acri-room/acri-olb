b1df77e4ec7cb0617c2924f7f6a393540f05425d
Takefumi MIYOSHI <miyo@wasamon.net>
2020-06-17 08:54:50 +0900

    to stop multiple VMs by stopvm.sh

--- vm-host/stopvm.sh
+++ vm-host/stopvm.sh
@@ -1,3 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
-VBoxManage controlvm $* acpipowerbutton
+for i in $*
+do
+    VBoxManage controlvm $i acpipowerbutton
+done
+
+for i in $*
+do
+    while [ -n "$(VBoxManage list runningvms | grep ${i})" ]
+    do
+        echo "wait for stop ${i}"
+        sleep 5
+    done
+done
