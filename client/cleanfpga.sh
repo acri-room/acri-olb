bdee15c260912ef368948b544fada0fe7e3b5f67
Takefumi MIYOSHI <miyo@wasamon.net>
2020-06-23 20:36:27 +0900

    add HOME env in cleanfpga.sh

--- client/cleanfpga.sh
+++ client/cleanfpga.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 cd /root/acri-olb/client
-date > cleanfpga.log
-/tools/Xilinx/Vivado/2019.2/bin/vivado -mode batch -source /tools/acri/write_dummy.tcl
+HOME=/root /tools/Xilinx/Vivado/2019.2/bin/vivado -mode batch -source /tools/acri/write_dummy.tcl
 
