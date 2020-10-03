d36d9878184dcadc94158f41f73ed692008977b6
Takefumi MIYOSHI <miyo@wasamon.net>
2020-10-03 19:08:40 +0900

    update cleanfpga.sh to check Vivado

--- client/cleanfpga.sh
+++ client/cleanfpga.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-HOME=/root
 VIVADO=/tools/Xilinx/Vivado/2019.2/bin/vivado
 
 while [ ! -e ${VIVADO} ]
@@ -13,21 +12,21 @@ key=`hostname -s | cut -c 4-`
 
 echo $key
 case ${key} in
-	"01" ) ${VIVADO} -mode batch -source config_board_1.tcl ;;
-	"02" ) ${VIVADO} -mode batch -source config_board_2.tcl ;;
-	"03" ) ${VIVADO} -mode batch -source config_board_3.tcl ;;
-	"04" ) ${VIVADO} -mode batch -source config_board_4.tcl ;;
-	"05" ) ${VIVADO} -mode batch -source config_board_5.tcl ;;
-	"06" ) ${VIVADO} -mode batch -source config_board_6.tcl ;;
-	"07" ) ${VIVADO} -mode batch -source config_board_7.tcl ;;
-	"08" ) ${VIVADO} -mode batch -source config_board_8.tcl ;;
-	"09" ) ${VIVADO} -mode batch -source config_board_9.tcl ;;
-	"10" ) ${VIVADO} -mode batch -source config_board_A.tcl ;;
-	"11" ) ${VIVADO} -mode batch -source config_board_B.tcl ;;
-	"12" ) ${VIVADO} -mode batch -source config_board_C.tcl ;;
-	"13" ) ${VIVADO} -mode batch -source config_board_D.tcl ;;
-	"14" ) ${VIVADO} -mode batch -source config_board_E.tcl ;;
-	"15" ) ${VIVADO} -mode batch -source config_board_F.tcl ;;
+	"01" ) HOME=/root ${VIVADO} -mode batch -source config_board_1.tcl ;;
+	"02" ) HOME=/root ${VIVADO} -mode batch -source config_board_2.tcl ;;
+	"03" ) HOME=/root ${VIVADO} -mode batch -source config_board_3.tcl ;;
+	"04" ) HOME=/root ${VIVADO} -mode batch -source config_board_4.tcl ;;
+	"05" ) HOME=/root ${VIVADO} -mode batch -source config_board_5.tcl ;;
+	"06" ) HOME=/root ${VIVADO} -mode batch -source config_board_6.tcl ;;
+	"07" ) HOME=/root ${VIVADO} -mode batch -source config_board_7.tcl ;;
+	"08" ) HOME=/root ${VIVADO} -mode batch -source config_board_8.tcl ;;
+	"09" ) HOME=/root ${VIVADO} -mode batch -source config_board_9.tcl ;;
+	"10" ) HOME=/root ${VIVADO} -mode batch -source config_board_A.tcl ;;
+	"11" ) HOME=/root ${VIVADO} -mode batch -source config_board_B.tcl ;;
+	"12" ) HOME=/root ${VIVADO} -mode batch -source config_board_C.tcl ;;
+	"13" ) HOME=/root ${VIVADO} -mode batch -source config_board_D.tcl ;;
+	"14" ) HOME=/root ${VIVADO} -mode batch -source config_board_E.tcl ;;
+	"15" ) HOME=/root ${VIVADO} -mode batch -source config_board_F.tcl ;;
 esac
 
 killall -9 hw_server
