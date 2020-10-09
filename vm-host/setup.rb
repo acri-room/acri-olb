46d46f677b9b7793f668ef012462d1ed64cb813f
Takefumi MIYOSHI <miyo@wasamon.net>
2020-10-09 14:42:43 +0900

    add vs?00 in setup.rb

--- vm-host/setup.rb
+++ vm-host/setup.rb
@@ -1,14 +1,14 @@
 #!/usr/bin/env ruby
 
 def generate(dst, key)
-  vm_num = 15
+  vm_num = 16
   if ['serv0', 'serv1', 'serv2', 'serv3', 'serv4', 'serv5'].include?(key) then
-    vm_num = 15
+    vm_num = 16 # 0-15
   elsif ['serv6'].include?(key) then
-    vm_num = 10
+    vm_num = 11 # 0-10
   end
   vm_num.times{|i|
-    vm = format("vs%s%02d", key[-1], i+1)
+    vm = format("vs%s%02d", key[-1], i)
     dst.puts("VBoxManage startvm #{vm} --type headless")
   }
 end
