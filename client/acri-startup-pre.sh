9c7fc8e2cec1cd0a4ab920740b45c1e91280e830
Naoki FUJIEDA <nf.longarch@gmail.com>
2023-04-26 16:36:42 +0900

    Finally we have the root user prepare temporary files

---
+++ client/acri-startup-pre.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+touch /tmp/digilent-adept2-shm-dvtbl
+touch /tmp/digilent-adept2-shm-dvtopn
+touch /tmp/digilent-adept2-shm-ftdevcmg
+touch /tmp/digilent-adept2-shm-ftdmgr
+touch /tmp/digilent-adept2-mtx-dvtopn
+touch /tmp/digilent-adept2-mtx-dvtbl
+touch /tmp/digilent-adept2-mtx-ftdevcmg
+touch /tmp/digilent-adept2-mtx-otinit
+touch /tmp/digilent-adept2-mtx-devlock00
+touch /tmp/digilent-adept2-mtx-devlock01
+touch /tmp/digilent-adept2-mtx-devlock02
+touch /tmp/digilent-adept2-mtx-devlock03
+touch /tmp/digilent-adept2-mtx-devlock04
+touch /tmp/digilent-adept2-mtx-devlock05
+touch /tmp/digilent-adept2-mtx-devlock06
+touch /tmp/digilent-adept2-mtx-devlock07
+touch /tmp/digilent-adept2-mtx-devlock08
+touch /tmp/digilent-adept2-mtx-devlock09
+touch /tmp/digilent-adept2-mtx-devlock0A
+chmod 777 /tmp/digilent-adept2-shm-dvtbl
+chmod 777 /tmp/digilent-adept2-shm-dvtopn
+chmod 777 /tmp/digilent-adept2-shm-ftdevcmg
+chmod 777 /tmp/digilent-adept2-shm-ftdmgr
+chmod 777 /tmp/digilent-adept2-mtx-dvtopn
+chmod 777 /tmp/digilent-adept2-mtx-dvtbl
+chmod 777 /tmp/digilent-adept2-mtx-ftdevcmg
+chmod 777 /tmp/digilent-adept2-mtx-otinit
+chmod 777 /tmp/digilent-adept2-mtx-devlock00
+chmod 777 /tmp/digilent-adept2-mtx-devlock01
+chmod 777 /tmp/digilent-adept2-mtx-devlock02
+chmod 777 /tmp/digilent-adept2-mtx-devlock03
+chmod 777 /tmp/digilent-adept2-mtx-devlock04
+chmod 777 /tmp/digilent-adept2-mtx-devlock05
+chmod 777 /tmp/digilent-adept2-mtx-devlock06
+chmod 777 /tmp/digilent-adept2-mtx-devlock07
+chmod 777 /tmp/digilent-adept2-mtx-devlock08
+chmod 777 /tmp/digilent-adept2-mtx-devlock09
+chmod 777 /tmp/digilent-adept2-mtx-devlock0A
