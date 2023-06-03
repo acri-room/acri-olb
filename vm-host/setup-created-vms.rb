f442b5657d8ad25b42fa6324db5817712c7fde26
Naoki FUJIEDA <nf.longarch@gmail.com>
2023-06-03 13:29:54 +0900

    Automate the assignment of Arty boards (not tested yet)

--- vm-host/setup-created-vms.rb
+++ vm-host/setup-created-vms.rb
@@ -21,8 +21,16 @@ end
 # start, wait, and stop a VM to setup
 ###########################################################
 
-def autosetup_vm(vmname)
+def autosetup_vm(vmname, usbserial = nil)
   puts "## Starting the auto setup process of #{vmname}"
+  if usbserial
+    puts "## Assigning a board with serial number of #{usbserial}"
+    system("VBoxManage modifyvm #{vmname} --usb off --usbxhci on")
+    system("VBoxManage usbfilter add 0 --target #{vmname} " +
+      '--name "Digilent USB Device [0700]" --vendorid 0403 --productid 6010 ' +
+      '--revision 0700 --manufacturer Digilent --product "Digilent USB Device" ' +
+      "--serialnumber #{usbserial}")
+  end
   system("VBoxManage startvm #{vmname} --type headless")
   while ! File.exist?(LOGFILE % vmname) # generated after vm-host-setup.py is completed
     wait_printing_dot
@@ -75,11 +83,22 @@ def main()
     exit 0
   end
 
+  # Enumerate USB devices
+  usb_devices = `VBoxManage list usbhost | grep SerialNumber`.strip.split("\n")
+  usb_devices = usb_devices.select{|x| x =~ /[0-9A-F]{12}$/ }.map{|x| x[-12..-1] }
+  usb_map = Hash.new
+  setup_vms.each do |vmname|
+    vmindex = vmname[-2..-1].to_i
+    if vmindex != 0 && vmindex <= usb_devices.size
+      usb_map[vmname] = usb_devices[vmindex - 1] 
+    end
+  end
+
   # Remove old setup log (if exists)
   FileUtils.rm_f(setup_vms.map{|vmname| LOGFILE % vmname })
 
   # Setup each of the VMs
-  setup_vms.each{|vmname| autosetup_vm(vmname) }
+  setup_vms.each{|vmname| autosetup_vm(vmname, usb_map[vmname]) }
 end
 
 main()
