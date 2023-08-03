2f31059c6d68508f3abbfc6edb04510444ff1bdf
Naoki FUJIEDA <nf.longarch@gmail.com>
2023-08-03 13:30:27 +0900

    add access restriction to the local http server

--- server/olb-server.rb
+++ server/olb-server.rb
@@ -3,9 +3,20 @@ require 'webrick'
 
 load 'config.rb'
 
-srv = WEBrick::HTTPServer.new({ :DocumentRoot => './',
+def address_allow?(addr)
+  addr == '127.0.0.1' ||
+  addr == '172.16.2.5' ||
+  addr.start_with?('172.16.3.') ||
+  addr.start_with?('172.16.5.') ||
+  addr.start_with?('172.16.6.')
+end
+
+srv = WEBrick::HTTPServer.new({ :DocumentRoot => './doc/',
                                 :BindAddress => '0.0.0.0',
                                 :Port => 20080})
-srv.mount('/olb-view.cgi', WEBrick::HTTPServlet::CGIHandler, 'olb-view.rb')
+srv.mount_proc('/olb-view.cgi') do |req, res|
+  raise WEBrick::HTTPStatus::Forbidden if ! address_allow?(req.peeraddr[3])
+  WEBrick::HTTPServlet::CGIHandler.new(srv, 'olb-view.rb').do_GET(req, res)
+end
 trap("INT"){ srv.shutdown }
 srv.start
